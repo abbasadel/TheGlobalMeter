d6101e4e6c9507cc07bdcce297cb14105ac10587
Abbas Adel <abbas.adel@mufix.org>
2013-07-25 21:38:56 +0200

    fix UI issues

--- www/app/views/site/home.blade.php
+++ www/app/views/site/home.blade.php
@@ -10,17 +10,17 @@ My Meter
 @section('content')
 <br />
 <div class="row-fluid" id="header">
-    <div class="span4" id="meter-description">{{$meter->description}}</div>
+    <div class="span4" id="meter-description"><blockquote>{{$meter->description}}</blockquote></div>
     <div class="span4"><img src="{{ '/' . $meter->logo}}" title="{{$meter->title}}" /></div>
     <div class="span4" id="meter-info">
         <div class="done"> 
-            <span class="number">8</span> Overall promises achieved
+            <span class="number">8</span> Overall promises <b>achieved</b>
         </div>
         <div class="inprogress"> 
-            <span class="number">12</span> Overall promises in progress
+            <span class="number">12</span> Overall promises <b>in progress</b>
         </div>
         <div class="notdone"> 
-            <span class="number">22</span> Overall promises not done
+            <span class="number">22</span> Overall promises <b>not done</b>
         </div>
 
     </div>
@@ -31,7 +31,7 @@ My Meter
 
     <div class="tabbable" id="catsTabs">
         <div>
-            <ul class="nav nav-tabs">
+            <ul class="nav nav-tabs" style="overflow: hidden">
                 @foreach ($meter->categories as $category)
                 <li>
                     <a href="#tab{{ $category->id }}" title="{{ $category->name }}"><h3><i class="{{$category->icon}}"></i><span class="title"> {{ $category->name }}</span></h3></a>
@@ -45,10 +45,14 @@ My Meter
 
                 <table class="table table-striped">
                     @foreach($category->promises as $promise)
-                    <tr>
-                        <td class="{{$promise->statusText()}}">
-                            <i class="{{$promise->statusIcon()}}"></i>
-                            {{$promise->name}}
+                    <tr class="promise">
+                        <td class="{{$promise->statusText()}} ">
+
+                            <p><i class="{{$promise->statusIcon()}}"></i>
+                                {{$promise->name}}</p>
+                            <blockquote class="description">
+                                {{$promise->description}}
+                            </blockquote>
                         </td>
                     </tr>
                     @endforeach
@@ -71,14 +75,25 @@ My Meter
     $('#catsTabs a').click(function(e) {
         e.preventDefault();
         $(this).tab('show');
-        $('#catsTabs a span.title').fadeOut(500);
-        $(this).find('span.title').fadeIn(1000);
+        $('#catsTabs a span.title').fadeOut(300);
+        $(this).find('span.title').show(1000);
     })
     $('#catsTabs a:first').tab('show'); // Select first tab
     $('#catsTabs a:first span.title').fadeIn(500);
 
 
-    //tabs hide effect
+    //promise description effect
+
+    $('tr.promise').click(
+            function() {
+                var status = $(this).find('.description').is(":visible")
+                if(status){
+                    $(this).find('.description').hide(200);
+                }else{
+                    $(this).find('.description').show(200);
+                }
+            });
+
 
 
 </script>
